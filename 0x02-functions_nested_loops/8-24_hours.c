bd0a2f435880be465125a9b0e80cd1f45b3d34a4
farouk <farouksigma2004@gmail.com>
2023-08-15 19:06:10 -0700

    Task 9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - prints every miutes of the day
+ * jack_bauer - prints every minutes of the day
  * Return: void
  */
 void jack_bauer(void)
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,23 @@
-#include
+#include "main.h"
+#include <stdio.h>
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			result = i * j;
+			if (j == 0)
+				printf("%d, ", result);
+			else
+			{
+				printf("%2d", result);
+				if (j != 9)
+					printf(", ");
+			}
+			printf("\n");
+		}
+	}
+}
